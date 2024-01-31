9369284c0118cdd31575da01b7a2268f4c982df5
sat-kamilw <kamil.warpechowski@sat.poznan.pl>
2024-01-31 17:58:34 +0100

    logo

--- app/wspolpraca/page.tsx
+++ app/wspolpraca/page.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image'
 
 export const metadata = {
   title: 'Współpraca',
@@ -6,7 +7,7 @@ export const metadata = {
 }
 
 async function getData() {
-  const res = await fetch(`https://admin.ipionierzy.pl/api/partner?populate=*`)
+  const res = await fetch(`https://admin.ipionierzy.pl/api/partner?populate%5Bpartners%5D%5Bpopulate%5D=*`)
   const { data } = await res.json()
   return data.attributes
 }
@@ -42,10 +43,18 @@ const Partners = async () => {
 
       <div className="relative max-w-6xl mx-auto p-10">
         <h2 className="text-2xl font-extrabold pb-5">Partnerzy</h2>
-        <ul className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-5">
-          {partners.map(({ id, title }: any) => {
+        <ul className="grid grid-cols-2 md:grid-cols-4 gap-5 mt-5">
+          {partners.map(({ id, title, logo }: any) => {
             return (
-              <li className="p-6 bg-white rounded-2xl shadow-sm" key={id}>{title}</li>
+              <li className="p-1 bg-white rounded-2xl shadow text-center" key={id}>
+                <Image
+                    src={logo.data.attributes.url}
+                    width={logo.data.attributes.width}
+                    height={logo.data.attributes.height}
+                    alt={title}
+                    className="pointer-events-none"
+                />
+                </li>
             )
           })}
         </ul>
